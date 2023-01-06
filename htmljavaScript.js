f059d732cca0dc179938bf4f12654236aedee2af
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-01-06 14:27:28 -0700

    Update htmljavaScript.js

--- htmljavaScript.js
+++ htmljavaScript.js
@@ -7,18 +7,25 @@ function OnClick(){
     document.getElementById("demo").innerHTML = result;
 }
 
-let number = 0
-
+let number = 0;
 function nightMode(){ 
+
+    let night = document.querySelector("button");
     if (number % 2 == 1){
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
         document.body.style.borderColor = "white";
+
+        night.style.backgroundColor="white";
+        night.style.borderColor = "white";
     }
     if (number % 2 == 0){
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
         document.body.style.borderColor = "black";
+
+        night.style.backgroundColor="black";
+        night.style.borderColor = "black";
     }
 
     number += 1;
