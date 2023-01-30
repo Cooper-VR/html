36b351fe1a4e073531a9db7c0c937a8f6f36fd92
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-01-30 12:46:09 -0700

    L1 done

---
+++ javascript/Main.js
@@ -0,0 +1,15 @@
+//Cooper Bower P.M.
+// JSF1 Assignment Variables and Values
+
+
+let country = "japan";
+let continent = "asia";
+let population = 1257000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let country2 = country;
+
+alert(country2)
