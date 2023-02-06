439532de3b615273b6a09d77a3bcee568d10a98f
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-02-06 14:33:51 -0700

    done

--- php/upload.php
+++ php/upload.php
@@ -1,44 +1,27 @@
 <?php
-if (isset($_FILES['file'])) {
-  $file = $_FILES['file'];
+    if (isset($_FILES['file']))
+    {
+        $file = $_FILES['file'];
+        $fileName = $file['name'];
+        $fileTempName = $file['tmp_name'];
+        $fileSize = $file['size'];
+        $fileError = $file['error'];
+        $fileType = $file['type'];
 
-    print_r($file);
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
 
-  $fileName = $file['name'];
-  $fileTmpName = $file['tmp_name'];
-  $fileSize = $file['size'];
-  $fileError = $file['error'];
-  $fileType = $file['type'];
+        $allowed = array("txt");
 
-  $fileExt = explode('.', $fileName);
-  $fileActualExt = strtolower(end($fileExt));
-
-  $allowed = array('jpg', 'jpeg', 'png', 'pdf');
-
-  if (in_array($fileActualExt, $allowed)) {
-    if ($fileError === 0) {
-      if ($fileSize < 1000000) {
-        $fileNameNew = uniqid('', true) . "." . $fileActualExt;
-        $fileDestination = 'uploads/' . $fileNameNew;
-
-        if (!file_exists('uploads')) {
-          mkdir('uploads', 0777, true);
+        if (in_array($fileActualExt, $allowed))
+        {
+            $fileDestination = 'upload/' . $fileName;
+            move_uploaded_file($fileTempName, $fileDestination);
+            echo "File uploaded successfully";
         }
-
-        $fileDestination = realpath('uploads') . '/' . $fileNameNew;
-
-        if (move_uploaded_file($fileTmpName, $fileDestination)) {
-          echo 'File uploaded successfully';
-        } else {
-          echo 'Unable to move the uploaded file to the destination directory';
+        else
+        {
+            echo "Error: Invalid file type";
         }
-      } else {
-        echo 'File is too large';
-      }
-    } else {
-      echo 'There was an error uploading your file';
     }
-  } else {
-    echo 'Invalid file type';
-  }
-}
+?>
